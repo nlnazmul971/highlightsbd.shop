ce694821f0de653075e2b006788e849752170056
nlnazmul971 <104690285+nlnazmul971@users.noreply.github.com>
2026-05-11 07:03:24 +0600

    fix

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -66,7 +66,7 @@ const ProductImageGallery = ({ mainImage, name, productId }: { mainImage: string
       {/* Main image */}
       <div
         ref={imgRef}
-        className="flex-1 aspect-[4/5] lg:h-[1000px] lg:max-h-[1000px] overflow-hidden bg-secondary cursor-crosshair relative border border-border"
+        className="flex-1 aspect-[2/3] lg:h-[1000px] lg:max-h-[1000px] overflow-hidden bg-secondary cursor-crosshair relative border border-border"
         onMouseEnter={() => setZoomed(true)}
         onMouseLeave={() => setZoomed(false)}
         onMouseMove={handleMouseMove}
